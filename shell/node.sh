29bb6954eed411981673029c2e0cd95ac5bd27f7
Daniel Weidner <hallo@danielweidner.de>
2017-10-17 14:25:36 +0200

    feat: Customize npm userconfig if file exists

--- shell/node.sh
+++ shell/node.sh
@@ -5,6 +5,10 @@
 
 # (1) Node Package Manager ---------------------------------------------- {{{1
 
+if [[ -r "${XDG_CONFIG_HOME}/npm/npmrc" ]]; then
+  export NPM_CONFIG_USERCONFIG="${XDG_CONFIG_HOME}/npm/npmrc"
+fi
+
 export NPM_CONFIG_INIT_VERSION="0.0.1"
 export NPM_CONFIG_INIT_LICENSE="MIT"
 export NPM_CONFIG_CACHE="${XDG_CACHE_HOME}/npm"
