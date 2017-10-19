ae1612d99b921cdf5637789b1e65f8065f308007
Daniel Weidner <hallo@danielweidner.de>
2017-10-19 16:52:39 +0200

    feat: Enable completion for git alias

--- bash/completion.sh
+++ bash/completion.sh
@@ -2,8 +2,10 @@
 #
 # @file Enable bash completion.
 
+
 # (1) System completions ------------------------------------------------ {{{1
 
+# Load bash completions installed in system paths
 if [[ -r "/usr/local/etc/bash_completion" ]]; then
   source "/usr/local/etc/bash_completion"
 elif [[ -r "/etc/bash_completion" ]]; then
@@ -13,10 +15,15 @@ fi
 
 # (2) Third Party completions ------------------------------------------- {{{1
 
+# Load bash completion package installed via homebrew
 if (( BASH_VERSINFO[0] >= 4 )); then
   [[ -r "${HOMEBREW_PREFIX}/share/bash-completion/bash_completion" ]] \
     && source "${HOMEBREW_PREFIX}/share/bash-completion/bash_completion"
 fi
 
+# Enable completion for the custom git alias
+if dot::function_exists "__git_complete"; then
+  __git_complete g __git
+fi
 
 # vim:foldmethod=marker:foldlevel=2
