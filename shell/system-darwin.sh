238736cd7b4b87025f5f98f5a8e117fc9d014d52
Daniel Weidner <hallo@danielweidner.de>
2017-10-20 10:48:11 +0200

    fix: Strip default value for XDG_APPLICATION_DIR

--- shell/system-darwin.sh
+++ shell/system-darwin.sh
@@ -2,12 +2,13 @@
 #
 # @file Environment variables for systems running macOS.
 
+
 export DOTFILES_OS="Darwin"
 
 export SHELL_SESSION_HISTORY=0
 
 export HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-/usr/local}"
-export HOMEBREW_CASK_OPTS="--appdir=${XDG_APPLICATION_DIR:-${HOME}/Applications}"
+export HOMEBREW_CASK_OPTS="--appdir=${XDG_APPLICATION_DIR}"
 
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_INSECURE_REDIRECT=1
