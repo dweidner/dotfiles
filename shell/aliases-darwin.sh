2ce107313c1549e4d247043592a3b24050faf89c
Daniel Weidner <hallo@danielweidner.de>
2017-10-20 10:46:57 +0200

    feat: Add aliases to macOS applications

--- shell/aliases-darwin.sh
+++ shell/aliases-darwin.sh
@@ -2,10 +2,20 @@
 #
 # @file Mac specific shell aliases.
 
-alias cdf='cd "$(printfd)"'    # Switch to directory selected in finder
-alias puf='pushd "$(printfd)"' # Push finder directory to the stack
 
-alias hide="chflags hidden"    # Hide a file in Finder
-alias show="chflags nohidden"  # Show a file in Finder
+alias cdf='cd "$(printfd)"'            # Switch to current Finder directory
+alias puf='pushd "$(printfd)"'         # Push Finder directory to the stack
 
-alias cask="brew cask"         # Easily install applications via homebrew
+alias ql="qlmanage -p >/dev/null 2>&1" # Open a file in quick look
+
+alias hide="chflags hidden"            # Hide a file in Finder
+alias show="chflags nohidden"          # Show a file in Finder
+
+alias cask="brew cask"                 # Easily install apps via homebrew
+alias reload="source ~/.bash_profile"  # Reload bash configuration
+
+alias finder="open -a Finder"          # Open a directory in Finder
+alias chrome="open -a Google\ Chrome"  # Open a website in Google Chrome
+alias firefox="open -a Firefox"        # Open a website in Firefox
+alias safari="open -a Safari"          # Open a website in Safari
+alias preview="open -a Preview"        # Display a preview of any type of file
