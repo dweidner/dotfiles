fb352e4582ba8de770681f119f8e42f37f8e834b
Daniel Weidner <hallo@danielweidner.de>
2017-11-01 13:33:01 +0100

    feat: Disable cdable_vars option to reduce noise

--- bash/options.sh
+++ bash/options.sh
@@ -10,12 +10,12 @@ stty -ixon                # Disable start/stop output control
 set -o notify             # Notify about terminating backround jobs
 
 shopt -s cdspell          # Correct typos in directory names when using cd
-shopt -s cdable_vars 	  # Change to a directory saved in a variable
+shopt -s cdable_vars      # Change to a directory saved in a variable
 shopt -s checkwinsize     # Ensure LINES and COLUMNS match the window size
-shopt -s cmdhist 	  # Join multi-line commands into one
+shopt -s cmdhist          # Join multi-line commands into one
 shopt -s dotglob          # Expand filenames starting with a dot
 shopt -s extglob          # Enable extended pattern matching for path expansion
-shopt -s histappend 	  # Append to history when the shell exits
+shopt -s histappend       # Append to history when the shell exits
 shopt -s hostcomplete     # Perform host completion for words containing @
 shopt -s nocaseglob       # Enable case-insensitive globbing
 
