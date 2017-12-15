e7a4fec656692706f8d78c58ffb66d80af444546
Daniel Weidner <hallo@danielweidner.de>
2017-12-15 17:34:04 +0100

    feat: Customize fzf settings

--- bash/fzf.sh
+++ bash/fzf.sh
@@ -2,6 +2,8 @@
 #
 # @file Load command-line fuzzy finder
 
+[[ -d "/usr/local/opt/fzf" ]] || return
+
 
 # (1) Path customization ------------------------------------------------ {{{1
 
@@ -26,13 +28,77 @@ if [[ -s "${FZF_DIR}/shell/key-bindings.bash" ]]; then
 fi
 
 
-# (4) Configuration ----------------------------------------------------- {{{1
+# (4) Fzf functions ...-------------------------------------------------- {{{1
 
-export FZF_DEFAULT_OPTS='
-  --height 40%
+# Open selected files with the default editor
+fe() {
+  local -a files=()
+
+  while IFS=$'\n' read -r file; do
+    files+=("$file")
+  done < <(fzf --query="$1" --multi --select-1 --exit-0)
+
+  if (( "${#files}" > 0 )); then
+    ${EDITOR:-vim} "${files[@]}"
+  fi
+}
+
+# Browse the git history using fzf
+fh() {
+  dot::in_git_repository || return
+
+  local hash="[a-f0-9]\\{7,\\}"
+  local preview="grep -o \"${hash}\" <<< {} | xargs git show --color | head -200"
+
+  git log --date=short --graph --color=always --pretty=nice |
+    fzf --query="$1" --ansi --no-sort --multi --preview "${preview}" |
+    grep -o "${hash}"
+}
+
+# Search ctags and open the selected file with the default editor
+ft() {
+  dot::in_git_repository || return
+
+  local git_dir
+  git_dir="$(git rev-parse --show-toplevel)/.git"
+
+  [[ -r "$git_dir/tags" ]] || return
+
+  local selection
+  selection="$(awk 'BEGIN { FS="\t" } !/^!/ {print toupper($4)"\t"$1"\t"$2}' "$git_dir/tags" | fzf --query="$1" --nth=2,3)"
+
+  local tag
+  tag="$(cut -f2 <<< "$selection")"
+
+  local file
+  file="$(cut -f3 <<< "$selection")"
+
+  if [[ -r "$git_dir/$file" ]]; then
+    ${EDITOR:-vim} "$git_dir/$file" -c "set cscopetag" -c "silent tag ${tag}"
+  fi
+}
+
+
+# (5) Configuration ----------------------------------------------------- {{{1
+
+export FZF_DEFAULT_OPTS="
   --reverse
+  --inline-info
   --color light
-'
+  --height 40%
+  --prompt '${PROMPT_SYMBOL:-❯} '
+  --preview-window hidden
+  --bind '?:toggle-preview'
+  --bind 'ctrl-g:jump-accept'
+"
+
+export FZF_CTRL_T_OPTS="
+  --preview '[[ \$(file --mime {}) =~ binary ]] && file -b {} || (highlight -O ansi -l {} || cat {}) 2>/dev/null | head -100 {}'
+"
+
+export FZF_ALT_C_OPTS="
+  --preview 'tree --noreport --dirsfirst -C {} | head -100'
+"
 
 
 # vim:foldmethod=marker:foldlevel=2
