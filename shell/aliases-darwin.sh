d9617de25a5c7ea681a4591fecc555d0ec78c330
Daniel Weidner <hallo@danielweidner.de>
2018-01-03 16:01:00 +0100

    feat: Additional macOS aliases

--- shell/aliases-darwin.sh
+++ shell/aliases-darwin.sh
@@ -6,12 +6,16 @@
 # Can’t remember the path to the iCloud directory? Me neither …
 alias icloud="cd ~/Library/Mobile\ Documents/com~apple~CloudDocs"
 
+# Delete all .DS_Store files recursively
+alias dsrm="find . -type f -name .DS_Store -delete"
+
 alias ql="qlmanage -p >/dev/null 2>&1" # Open a file in quick look
 
 alias hide="chflags hidden"            # Hide a file in Finder
 alias show="chflags nohidden"          # Show a file in Finder
 
 alias cask="brew cask"                 # Easily install apps via homebrew
+alias services="brew services"         # Easily maintain homebrew services
 alias reload="source ~/.bash_profile"  # Reload bash configuration
 
 alias finder="open -a Finder"          # Open a directory in Finder
