8dda1efd124554e177255144844f5263a9b4afdd
Daniel Weidner <hallo@danielweidner.de>
2018-01-03 16:02:02 +0100

    feat: Copy to clipboard aliases for linux distros

--- shell/aliases-linux.sh
+++ shell/aliases-linux.sh
@@ -3,4 +3,9 @@
 # @file Linux specific shell aliases.
 
 
-alias reload="source ~/.bashrc"  # Reload bash configuration
+alias reload="source ~/.bashrc"                  # Reload bash configuration
+
+if command -v xclip >/dev/null 2>&1; then
+  alias pbcopy="xclip -selection clipboard"      # Copy to clipboard
+  alias pbpaste="xclip -selection clipboard -o"  # Paste from clipboard
+fi
