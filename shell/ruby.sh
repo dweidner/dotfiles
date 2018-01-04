7c9497037089b5350a283488d27f0308967cc5d1
Daniel Weidner <hallo@danielweidner.de>
2018-01-04 15:25:15 +0100

    feat: Add aliases for ruby bundler

--- shell/ruby.sh
+++ shell/ruby.sh
@@ -3,11 +3,16 @@
 # @file Configuration of the Ruby environment.
 
 
-export RVM_PATH="${XDG_DATA_HOME}/rvm"
+# (1) Ruby Configuration ------------------------------------------------ {{{1
 
 if [[ -r "${HOMEBREW_PREFIX}/opt/openssl" ]]; then
   export RUBY_CONFIGURE_OPTS="--with-openssl-dir=${HOMEBREW_PREFIX}/opt/openssl"
 fi
+
+
+# (2) Ruby Version Manager ---------------------------------------------- {{{1
+
+export RVM_PATH="${XDG_DATA_HOME}/rvm"
 
 if [[ -r "${RVM_PATH}/scripts/rvm" ]]; then
   source "${RVM_PATH}/scripts/rvm"
@@ -16,5 +21,14 @@ fi
 if [[ -r "${RVM_PATH}/scripts/completion" ]]; then
   source "${RVM_PATH}/scripts/completion"
 fi
+
+
+# (3) Ruby Bundler ------------------------------------------------------ {{{1
+
+alias be="bundle exec"
+alias bi="bundle install"
+alias bl="bundle list"
+alias bu="bundle update"
+
 
 # vim:foldmethod=marker:foldlevel=2
