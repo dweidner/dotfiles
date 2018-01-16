faaad892f4ca0ff8312499b3f6da0d652bc781d7
Daniel Weidner <hallo@danielweidner.de>
2018-01-16 12:11:25 +0100

    feat: Add python specific aliases

---
+++ shell/python.sh
@@ -0,0 +1,20 @@
+# shell/python.sh
+#
+# @file Configuration of the Python environment.
+
+
+# (1) Python Package Manager -------------------------------------------- {{{1
+
+if dot::command_exists "pip"; then
+  eval "$(pip completion --bash)"
+fi
+
+
+# (3) Python Versions --------------------------------------------------- {{{1
+
+alias py="python"
+alias py2="python2"
+alias py3="python3"
+
+
+# vim:foldmethod=marker:foldlevel=2
