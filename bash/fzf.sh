9fd76b20c7674f08f253f80c3e52788b24b85e0f
Daniel Weidner <hallo@danielweidner.de>
2018-02-07 14:12:33 +0100

    feat: Include hidden files when search with ripgrep

--- bash/fzf.sh
+++ bash/fzf.sh
@@ -31,9 +31,9 @@ fi
 # (4) Configuration ----------------------------------------------------- {{{1
 
 if dot::command_exists "rg"; then
-  export FZF_DEFAULT_COMMAND='rg --files --no-ignore-vcs --glob ""'
+  export FZF_DEFAULT_COMMAND='rg --files --hidden --no-ignore-vcs --glob ""'
 elif dot::command_exists "ag"; then
-  export FZF_DEFAULT_COMMAND='ag --skip-vcs-ignores -g ""'
+  export FZF_DEFAULT_COMMAND='ag --hidden --skip-vcs-ignores -g ""'
 else
   export FZF_DEFAULT_COMMAND='find . -path "*/\.*" -prune -o -type f -print -o -type l -print | sed s/^..//'
 fi
