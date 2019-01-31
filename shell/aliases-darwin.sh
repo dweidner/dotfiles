952ba7c989f48d378d9c46c2f37d40350f85f244
Daniel Weidner <d.weidner@hks-agentur.de>
2019-01-31 12:32:23 +0100

    refactor: Cleanup osx aliases

--- shell/aliases-darwin.sh
+++ shell/aliases-darwin.sh
@@ -3,14 +3,6 @@
 # @file Mac specific shell aliases.
 
 
-# Delete all .DS_Store files recursively
-alias rmds="find . -type f -name .DS_Store -delete"
-
-alias ql="qlmanage -p >/dev/null 2>&1"   # Open a file in quick look
-
-alias hide="chflags hidden"              # Hide a file in Finder
-alias show="chflags nohidden"            # Show a file in Finder
-
 alias cask="brew cask"                   # Easily install apps via homebrew
 alias services="brew services"           # Easily maintain homebrew services
 alias reload="source ~/.bash_profile"    # Reload bash configuration
@@ -21,6 +13,7 @@ alias chrome="open -a Google\ Chrome"    # Open a website in Google Chrome
 alias firefox="open -a Firefox"          # Open a website in Firefox
 alias safari="open -a Safari"            # Open a website in Safari
 alias preview="open -a Preview"          # Display a preview of any file
+alias ql="qlmanage -p >/dev/null 2>&1"   # Open a file in quick look
 
 alias dce="docker-compose exec"          # Run a command in a given service
 alias dcr="docker-compose restart"       # Restart a given service
