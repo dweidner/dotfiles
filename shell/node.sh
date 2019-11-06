37e07be4addb5afb1c364c2398621491ad3f516b
Daniel Weidner <d.weidner@hks-agentur.de>
2019-11-06 11:35:47 +0100

    fix: Load nvm completion only in bash shells

--- shell/node.sh
+++ shell/node.sh
@@ -22,7 +22,7 @@ if [[ -r "${NVM_DIR}/nvm.sh" ]]; then
   source "${NVM_DIR}/nvm.sh"
 fi
 
-if [[ -r "${NVM_DIR}/bash_completion" ]]; then
+if [[ -n "${BASH}" ]] && [[ -r "${NVM_DIR}/bash_completion" ]]; then
   source "${NVM_DIR}/bash_completion"
 fi
 
