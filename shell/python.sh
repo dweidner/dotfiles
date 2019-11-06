84c6468034afdb7e9601dd5a843a1c80ca02c0c9
Daniel Weidner <d.weidner@hks-agentur.de>
2019-11-06 11:36:52 +0100

    fix: Load correct pip completion for various shells

--- shell/python.sh
+++ shell/python.sh
@@ -5,14 +5,17 @@
 
 # (1) Python Package Manager -------------------------------------------- {{{1
 
-if dot::command_exists "pip"; then
-  eval "$(pip -qq --disable-pip-version-check completion --bash)"
+if dot::command_exists "pip3"; then
+  case "$SHELL_NAME" in
+    bash) eval "$(pip3 completion --bash)" ;;
+    zsh)  eval "$(pip3 completion --zsh)" ;;
+  esac
 fi
 
 
 # (3) Python Versions --------------------------------------------------- {{{1
 
-alias py="python"
+alias py="python3"
 alias py2="python2"
 alias py3="python3"
 
