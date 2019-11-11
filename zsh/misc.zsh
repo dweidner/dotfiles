237755a38a351a45d7c2540e5b50346d7e2a2e68
Daniel Weidner <d.weidner@hks-agentur.de>
2019-11-11 11:45:42 +0100

    feat: Add additional suffix aliases

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -7,6 +7,7 @@
 # -----------------------------------------------------------------------
 # 1. Abbreviations
 # 2. Online Help
+# 3. Suffix Aliases
 # -----------------------------------------------------------------------
 
 # (1) Abbreviations ----------------------------------------------------- {{{1
@@ -22,19 +23,27 @@ abbreviations=(
 )
 
 
-# (2) Suffix Aliases ---------------------------------------------------- {{{1
-
-alias -s log="tail -f" # Open log files with tail
-alias -s md="c"        # Display the contents of a markdown file in bat/cat
-alias -s txt="c"       # Display the contents of a text file in bat/cat
-
-
 # (2) Online Help ------------------------------------------------------- {{{1
 
 unalias run-help
 autoload run-help
 
 alias help="run-help"
+
+
+# (3) Suffix Aliases ---------------------------------------------------- {{{1
+
+# Preview text documents using cat/bat
+for ft in markdown md txt; do
+  alias -s "${ft}"="c"
+done
+
+# Display log files using tail
+alias -s log="tail -f"
+
+# List contents of zip files
+alias -s zip="unzip -l"
+alias -s tar="tar -tf"
 
 
 # vim:foldmethod=marker:foldlevel=2
