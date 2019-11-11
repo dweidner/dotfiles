729052b84d5757af2cfabe7e8f4f2c8935906d0b
Daniel Weidner <d.weidner@hks-agentur.de>
2019-11-11 11:45:51 +0100

    feat: Add macOS aliases

--- shell/aliases-darwin.sh
+++ shell/aliases-darwin.sh
@@ -8,13 +8,15 @@ alias services="brew services"           # Easily maintain homebrew services
 
 alias ql="qlmanage -p >/dev/null 2>&1"   # Open a file in quick look
 
-alias vscode="code"                      # Custom alias for VS Code
 alias marked="open -a Marked\ 2"         # Custom alias for Marked 2
 alias finder="open -a Finder"            # Open a directory in Finder
+alias preview="open -a Preview"          # Display a preview of any file
 alias chrome="open -a Google\ Chrome"    # Open a website in Google Chrome
 alias firefox="open -a Firefox"          # Open a website in Firefox
 alias safari="open -a Safari"            # Open a website in Safari
-alias preview="open -a Preview"          # Display a preview of any file
+
+alias browser="firefox"                  # Open firefox as default browser
+alias vscode="code"                      # Custom alias for VS Code
 
 alias dce="docker-compose exec"          # Run a command in a given service
 alias dcr="docker-compose run --rm"      # Run a one off command in a container
