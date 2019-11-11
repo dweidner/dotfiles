7cce952a83d3b6d2b0eef7037cdc232602224929
Daniel Weidner <d.weidner@hks-agentur.de>
2019-11-11 10:21:55 +0100

    feat: Add an alias for Marked 2

--- shell/aliases-darwin.sh
+++ shell/aliases-darwin.sh
@@ -5,15 +5,16 @@
 
 alias cask="brew cask"                   # Easily install apps via homebrew
 alias services="brew services"           # Easily maintain homebrew services
-alias reload="source ~/.bash_profile"    # Reload bash configuration
+
+alias ql="qlmanage -p >/dev/null 2>&1"   # Open a file in quick look
 
 alias vscode="code"                      # Custom alias for VS Code
+alias marked="open -a Marked\ 2"         # Custom alias for Marked 2
 alias finder="open -a Finder"            # Open a directory in Finder
 alias chrome="open -a Google\ Chrome"    # Open a website in Google Chrome
 alias firefox="open -a Firefox"          # Open a website in Firefox
 alias safari="open -a Safari"            # Open a website in Safari
 alias preview="open -a Preview"          # Display a preview of any file
-alias ql="qlmanage -p >/dev/null 2>&1"   # Open a file in quick look
 
 alias dce="docker-compose exec"          # Run a command in a given service
 alias dcr="docker-compose run --rm"      # Run a one off command in a container
