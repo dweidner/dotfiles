74726e6b71a0c17477e7bbdc22334bb11f653442
Daniel Weidner <d.weidner@hks-agentur.de>
2019-11-13 15:04:38 +0100

    refactor: Remove pbcopy aliases

--- shell/aliases-linux.sh
+++ shell/aliases-linux.sh
@@ -2,10 +2,3 @@
 #
 # @file Linux specific shell aliases.
 
-
-alias reload="source ~/.bashrc"                  # Reload bash configuration
-
-if dot::command_exists "xclip"; then
-  alias pbcopy="xclip -selection clipboard"      # Copy to clipboard
-  alias pbpaste="xclip -selection clipboard -o"  # Paste from clipboard
-fi
