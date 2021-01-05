5919a33e9a96435855669aa7b47bb2cd30f5159c
Daniel Weidner <d.weidner@hks-agentur.de>
2021-01-05 14:10:39 +0100

    feat: disable bash/zsh sessions in Terminal.app

--- shell/variables-zsh.sh
+++ shell/variables-zsh.sh
@@ -5,5 +5,6 @@
 export SHELL_NAME="zsh"
 export SHELL_VERSION="${ZSH_VERSION}"
 export SHELL_THEME="${SHELL_THEME:-pure}"
+export SHELL_SESSIONS_DISABLE=${SHELL_SESSIONS_DISABLE:-1}
 
-export HISTFILE="${XDG_DATA_HOME}/zsh/history"
+export HISTFILE="${XDG_DATA_HOME:-$HOME/.local/share}/zsh/history"
