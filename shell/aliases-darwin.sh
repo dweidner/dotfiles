14851eff9df9ad925b822ab0301517889400ed20
Daniel Weidner <d.weidner@hks-agentur.de>
2021-01-06 14:13:32 +0100

    feat: add alias for lazydocker

--- shell/aliases-darwin.sh
+++ shell/aliases-darwin.sh
@@ -19,9 +19,6 @@ keg_only=(
   "${HOMEBREW_PREFIX}/opt/curl-openssl/bin/curl"
 )
 
-alias cask="brew cask"                   # Easily install apps via homebrew
-alias services="brew services"           # Easily maintain homebrew services
-
 for cmd in "${keg_only[@]}"; do
   if [[ -x "${cmd}" ]]; then
     alias -- "${cmd##*/}"="${cmd}"
@@ -36,6 +33,8 @@ alias dcr="docker-compose run --rm"      # Run a one off command in a container
 alias dcu="docker-compose up -d --build" # Build, start and attach to a service
 alias dcd="docker-compose down"          # Stop and remove containers
 
+alias lzd="lazydocker"                   # Open a terminal UI for docker
+
 
 # (3) macOS Applications ------------------------------------------------ {{{1
 
@@ -49,3 +48,6 @@ alias safari="open -a Safari"            # Open a website in Safari
 alias ql="qlmanage -p >/dev/null 2>&1"   # Open a file in quick look
 alias browser="firefox"                  # Open firefox as default browser
 alias vscode="code"                      # Custom alias for VS Code
+
+
+# vim:foldmethod=marker:foldlevel=2
