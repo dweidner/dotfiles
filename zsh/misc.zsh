4753d2e3150dde4d8b0501daaa6244c0b437daed
Daniel Weidner <d.weidner@hks-agentur.de>
2021-01-12 14:37:31 +0100

    refactor: remove seldomly suffix aliases

--- zsh/misc.zsh
+++ zsh/misc.zsh
@@ -6,7 +6,6 @@
 # Table of Contents
 # -----------------------------------------------------------------------
 # 1. Online Help
-# 2. Suffix Aliases
 # -----------------------------------------------------------------------
 
 
@@ -16,21 +15,6 @@ unalias run-help
 autoload run-help
 
 alias help="run-help"
-
-
-# (3) Suffix Aliases ---------------------------------------------------- {{{1
-
-# Preview text documents using cat/bat
-for ft in markdown md txt; do
-  alias -s "${ft}"="c"
-done
-
-# Display log files using tail
-alias -s log="tail -f"
-
-# List contents of zip files
-alias -s zip="unzip -l"
-alias -s tar="tar -tf"
 
 
 # vim:foldmethod=marker:foldlevel=2
