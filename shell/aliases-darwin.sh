c36d0907ea52767fbab080efc76fbb7760431578
Daniel Weidner <hallo@danielweidner.de>
2021-04-28 19:49:37 +0200

    feat: add laravel sail alias

--- shell/aliases-darwin.sh
+++ shell/aliases-darwin.sh
@@ -33,6 +33,7 @@ alias dcr="docker-compose run --rm"      # Run a one off command in a container
 alias dcu="docker-compose up -d --build" # Build, start and attach to a service
 alias dcd="docker-compose down"          # Stop and remove containers
 
+alias sail="./vendor/bin/sail"           # Run a local copy of Laraval Sail
 alias lzd="lazydocker"                   # Open a terminal UI for docker
 
 
