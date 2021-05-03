4468d1895108473f33df33e69d2ca5e09c732fc2
Daniel Weidner <d.weidner@hks-agentur.de>
2021-05-03 12:37:26 +0200

    chore: update docker aliases

--- shell/aliases-darwin.sh
+++ shell/aliases-darwin.sh
@@ -28,10 +28,10 @@ done
 
 # (2) Docker ------------------------------------------------------------ {{{1
 
-alias dce="docker-compose exec"          # Run a command in a given service
-alias dcr="docker-compose run --rm"      # Run a one off command in a container
-alias dcu="docker-compose up -d --build" # Build, start and attach to a service
-alias dcd="docker-compose down"          # Stop and remove containers
+alias dce="docker compose exec"          # Run a command in a given service
+alias dcr="docker compose run --rm"      # Run a one off command in a container
+alias dcu="docker compose up -d --build" # Build, start and attach to a service
+alias dcd="docker compose down"          # Stop and remove containers
 
 alias sail="./vendor/bin/sail"           # Run a local copy of Laraval Sail
 alias lzd="lazydocker"                   # Open a terminal UI for docker
